9434a4ee4108bb9887328edfba051073a3a8ba5e
Adolfo Martínez <82050706+DempseyRolls@users.noreply.github.com>
2025-05-13 17:40:31 -0400

    pantalla para seleccionar tipo de perfil

---
+++ app/src/main/java/com/example/petcareapp/ProfileTypeSelectionActivity.kt
@@ -0,0 +1,13 @@
+package com.example.petcareapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProfileTypeSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
