9dcb6e59a7c9858f994a55962f3c244abd220d4c
Adolfo Martínez <82050706+DempseyRolls@users.noreply.github.com>
2025-05-19 22:43:11 -0400

    se agrego primera pantalla de registro del cuidador

--- app/src/main/java/com/example/petcareapp/RegistroCuidadorNombreFotoActivity.kt
+++ app/src/main/java/com/example/petcareapp/RegistroCuidadorNombreFotoActivity.kt
@@ -1,6 +1,8 @@
 package com.example.petcareapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,21 @@ class RegistroCuidadorNombreFotoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_registro_cuidador_nombre_foto)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registroCuidador1)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // --- Encontrar las vistas por su ID (ahora sí existirán después de setContentView)
+        val buttonSiguiente: ImageButton = findViewById(R.id.btnSiguienteCuidadorNombreFoto)
+
+        //Boton siguiente
+        buttonSiguiente.setOnClickListener {
+            //Intent para navegar a RegistroDuenioDatosActivity
+            val intent = Intent(this, RegistroDuenioDatosActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
